4b150af28a0300e02c4ce31f069941ebfaa7bec3
jonathan <79947845+blckjmmy@users.noreply.github.com>
2021-06-29 15:42:29 -0500

    added js.app toand and script to my index.html

--- app.js
+++ app.js
@@ -8,23 +8,37 @@
 
 // userName = 'Code'
 // console.log(userName)
-
+let ninja1;
+function getuserName(){
  let userName = prompt('what is username first name')
-console.log(userName)
-document.write('<h2>' + userName + '</h2>');
-
-let userAnswer = prompt('is naruto great show?');
-console.log(userAnswer)
-
- 
-
-if (userAnswer == 'yes') {
-    document.write('<h1>welcome Great Hokage ' + userName + '</h1>');
-
+ ninja1 = userName
+ console.log(userName)
+ document.write('<h2>' + userName + '</h2>');
+return userName;
 }
 
-if (userAnswer == 'no') {
-    document.write('<h1>leave village' + userName + '</h1>')
 
+function getuserAnswer(){ 
+    let userAnswer = prompt('is naruto great show?');
+    console.log(userAnswer)
+    
+    if (userAnswer == 'yes') {
+        document.write('<h1>Welcome Great Hokage ' + ninja1 + '</h1>');
+    }
+    
+    if (userAnswer == 'no') {
+        document.write('<h1>Leave Village' + ninja1 + '</h1>')
+
+    }
 }
+// let ninja1 = getuserName();
+// console.log(ninja1);
+
+// getuserAnswer();
+    
+
+
+
+
+
 
